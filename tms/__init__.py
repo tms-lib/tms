af47835e95d20e0e0946e5e05ff823219af91e9d
Alexanyan Andron <54064302+andron23@users.noreply.github.com>
2024-07-18 23:16:42 +0300

    upd

--- tms/__init__.py
+++ tms/__init__.py
@@ -2,6 +2,7 @@ import warnings
 
 import numpy as np
 import pandas as pd
+import requests
 
 from . import eda, interpolation, prediction, smoothing, stats
 
@@ -10,7 +11,7 @@ warnings.filterwarnings("ignore")
 
 class TimeSeries(np.ndarray):
     """Базовый класс TimeSeries, который является основным строительным блоком библиотеки.
-    
+
     Класс является наследником основного класса библиотеки Numpy - `np.ndarray`, что обеспечивает совместимость со всеми бибилотеками анализа данных.
     """
 
@@ -18,7 +19,7 @@ class TimeSeries(np.ndarray):
         """С помощью метода `__new__` создается объект класса `TimeSeries`, который является наследником `np.ndarray`.
 
         Параметры:
-        
+
         * input_data (array_like): Данные временного ряда - столбец датафрейма pandas, массив numpy, файл xlsx/csv, данные из txt, список, кортеж, множество питон.
         * dtype (data-type, optional): Ожидаемый тип данных. Defaults to float.
         """
@@ -34,6 +35,9 @@ class TimeSeries(np.ndarray):
             if input_data.endswith(".xlsx"):
                 df = pd.read_excel(input_data)
                 obj = np.asarray(df.iloc[:, 0].values, dtype=dtype).view(cls)
+            elif "http" in input_data:
+                data = requests.get(input_data).text.strip()
+                obj = np.asarray(eval(data), dtype=dtype).view(cls)
             elif input_data.endswith(".csv"):
                 df = pd.read_csv(input_data)
                 obj = np.asarray(df.iloc[:, 0].values, dtype=dtype).view(cls)
@@ -57,13 +61,13 @@ class TimeSeries(np.ndarray):
         Этот метод позволяет реализовать "принцип слоев", который лежит в основе библиотеки.
 
         Параметры:
-        
+
         * func (function): Функция, которую нужно применить к исходному временному ряду
         * `*args`: Произвольные позиционные аргументы, которые будут переданы в функцию.
         * `**kwargs`: Произвольные именованные аргументы, которые будут переданы в функцию.
 
         Возвращает:
-        
+
         * TimeSeries: Возвращается объект класса `TimeSeries`, который является результатом применения указанной функции к исходному временному ряду.
         """
         return TimeSeries(func(self, *args, **kwargs), self.dtype)
